629fc422bda6dffd3f8cb950ec7571410ced23d1
abhangale <abhangale@lockstephq.com>
2021-04-18 13:01:16 +0530

    Adding first git commit for my selenium code

---
+++ src/main/java/com/qa/utilities/Util.java
@@ -0,0 +1,8 @@
+package com.qa.utilities;
+
+public class Util {
+public void commomutil()
+{
+	System.out.println("util");
+}
+}
---
+++ src/test/java/com/qa/tests/Test.java
@@ -0,0 +1,8 @@
+package com.qa.tests;
+
+public class Test {
+public void Test()
+{
+	System.out.println("Test");
+}
+}
